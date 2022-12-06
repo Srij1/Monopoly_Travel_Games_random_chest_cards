e26b9c6fd95c03f18e3f55e01275210dee1b39b8
Srij1 <90286700+Srij1@users.noreply.github.com>
2022-12-06 02:52:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random
+
 community_chest = (3,4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18)
 Chance = (3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)
-Choice_of_chest = input(("To choose Community chest, type: chest, to choose chance, type chance."))
+
+Choice_of_chest = input(("To choose Community chest, type: 'chest', to choose chance, type 'chance'."))
 
 if Choice_of_chest == "chest":
   Community_Chest_Random = random.choice(community_chest)
-  print(Community_Chest_Random)
-  
+  print(Community_Chest_Random)    
+
 if Choice_of_chest == "chance":
   Chance_random = random.choice(Chance)
   print(Chance_random)
